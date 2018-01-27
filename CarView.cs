e684d9f67fe703b6b4c7e2e2a5924f0b65f8ce7e
Ian Rapoport <irapopor@ucsc.edu>
2018-01-26 22:09:24 -0800

    first draft of CarView.cs

---
+++ CarView.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarView : MonoBehaviour {
+
+	public AudioSource smokeSound;
+
+	private ParticleSystem smokeEmitter;
+	private CarModel carModel;
+	private bool smoking;
+
+	void Awake(){
+		smokeEmitter = GetComponent <ParticleEmitter> ();
+		carModel = GetComponent <CarModel> ();
+		smoking = false;
+	}
+
+	void Update (){
+		if (carModel.SpaceDown () && ! smoking) {
+			smoking = true;
+			smokeEmitter.Play ();
+			smokeSound.Play ();
+		} else if (smoking) {
+			smoking = false;
+			smokeEmitter.Pause ();
+			smokeSound.Stop ();
+		}
+	}
+}
