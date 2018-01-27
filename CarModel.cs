0cb15addc068d812656f22d1d176221cc46bb923
unknown <cfocht@ucsc.edu>
2018-01-27 10:04:53 -0800

    car moves forward and turns

---
+++ CarModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarModel : MonoBehaviour {
+
+	public float speed;
+	private Vector3 direction;
+	
+	void Start()
+	{
+		direction = Vector3.zero;
+	}
+	
+	void Update()
+	{
+		transform.Translate(Vector3.right * speed * Time.deltaTime);
+		transform.Rotate(direction);
+	}
+	
+	public void setDirection(Vector3 vector)
+	{
+		direction = vector;
+	}
+}
